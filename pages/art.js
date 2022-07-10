b4f39651567c34ce62c687acecd70b9265efd50e
Vincent Trélat <vincent.trelat@free.fr>
2022-07-10 14:30:18 +0200

    Updates

--- pages/art.js
+++ pages/art.js
@@ -1,28 +1,61 @@
-import React from "react";
-
-const getURLs = () => {
-    const baseURL =
-        "https://raw.githubusercontent.com/VTrelat/website/main/img/art/";
-    let out = [];
-    for (let i = 0; i <= 23; i++) {
-        const url = `${baseURL}0${i}.jpg`;
-        out.push(url);
-    }
-
-    return out;
-};
-const images = getURLs();
-
-console.log(images);
+import React, { useState } from "react";
 
 const Art = () => {
-    const currentFrame = (index) => null;
+    const [pos, setPos] = useState({ x: 0, y: 0, scale: 8 });
+    const [isDragging, setIsDragging] = useState(true);
+
+    const onScroll = (e) => {
+        if (pos.scale === 1) {
+            setIsDragging(false);
+        } else if (pos.scale > 1) {
+            setIsDragging(true);
+        }
+        const delta = e.deltaY * -0.005;
+        const newScale = pos.scale + delta;
+
+        const ratio = 1 - newScale / pos.scale;
+
+        setPos({
+            scale: newScale < 1 ? 1 : newScale > 8 ? 8 : newScale,
+            x: pos.x,
+            y: pos.y,
+        });
+    };
 
     return (
-        <main id="artpage" style={{ color: "white" }}>
-            {/* <canvas class="photo-scrolling"></canvas> */}
-            <section className="header">Empty for now</section>
-            <img src="https://raw.githubusercontent.com/VTrelat/website/main/img/art/010.jpg" />
+        <main
+            className="artpage"
+            style={{
+                height: isDragging ? "100vh" : "",
+                overflow: isDragging ? "hidden" : "auto",
+                color: "white",
+            }}
+        >
+            <img
+                src="https://raw.githubusercontent.com/VTrelat/website/main/img/art/00.jpg"
+                onWheelCapture={onScroll}
+                style={{
+                    height: "auto",
+                    width: "100%",
+                    transformOrigin: "49% 58%",
+                    transform: `scale(${pos.scale}) translate(${pos.x}px, ${pos.y}px)`,
+                    mask: `linear-gradient(rgba(255,255,255,1) ${
+                        60 * (1 - (pos.scale - 1) / 7)
+                    }%, rgba(255,255,255,${(pos.scale - 1) / 7}))`,
+                }}
+            />
+            <section
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    height: "100vh",
+                    width: "100%",
+                    fontSize: "4em",
+                }}
+            >
+                Art & Photography
+            </section>
         </main>
     );
 };
